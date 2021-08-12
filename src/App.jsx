288a471c47e4c41059436ca4cf94c01324a076d7
bnn848 <77334083+bnn848@users.noreply.github.com>
2021-08-12 16:28:19 +0900

    複数の状態管理

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [count, setCount]  = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const onReset = () => setCount(0)
-  const three = () => {
-    if (count % 3 === 0) {
-      setCount(count / 3)
-    }
+
+  /**
+   * (1)初期値をまとめてオブジェクトで定義する
+   */
+  const initialState = {
+    name: 'サンプル',
+    price: 1000
   };
+
+  /**
+   * (2)状態管理はHooksで記述する
+   */
+  const [name, setName] = useState(initialState.name)
+  const [price, setPrice] = useState(initialState.price)
   
   return (
     <React.Fragment>
-    <div>
-      count: {count}
-    </div>
-    <button onClick={increment}>+1</button>
-    <button onClick={decrement}>-1</button>
-    <div>
-    <button onClick={onReset}>Reset</button>
-    <button onClick={three}>3の倍数の時だけ３で割る</button>
-    </div>
+      <p>現在の{name}は、{price}です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={() => {
+        setPrice(initialState.price)
+        setName(initialState.name)
+      }}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </React.Fragment>
   );
 };
