4df04cef65bfb1d7c3fa1379b4392b3c4170c0b2
bnn848 <ryota.hashiba0426@gmail.com>
2021-08-14 14:39:21 +0900

    ReducerEventの作成

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,30 +9,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
         <form>
           <div className="form-group">
-            <label htmlFor="formEventTitle">Title</label>
+            <label htmlFor="formEventTitle">タイトル</label>
             <input type="text" className="form-control" id="formEventTitle" />
           </div>
           <div className="form-group">
-            <label htmlFor="formEventBody">Body</label>
+            <label htmlFor="formEventBody">内容</label>
             <input type="text" className="form-control" id="formEventBody" />
           </div>
 
-          <button className="btn btn-primary">Create Event</button>
-          <button className="btn btn-danger">Delete All Events</button>
+          <button className="btn btn-primary">イベントを作成</button>
+          <button className="btn btn-danger">全てのイベントを削除</button>
         </form>
 
         <h4>Events Table</h4>
         <table className="table table-hover">
           <thead>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Body</th>
-            <th></th>
+            <tr>
+              <th>ID</th>
+              <th>タイトル</th>
+              <th>内容</th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
-            <tr>
-              <td></td>
-            </tr>
           </tbody>
         </table>
       </div>
