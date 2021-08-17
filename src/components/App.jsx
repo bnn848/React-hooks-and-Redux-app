d2258eaaff3e0c39aa88122a0cb8d803d39f8f09
bnn848 <ryota.hashiba0426@gmail.com>
2021-08-17 16:09:39 +0900

    localStorageを利用してデータを保存する

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import reducer from '../reducer';
@@ -7,16 +7,23 @@ import EventForm from './EventForm';
 import Events from './Events';
 import OperationLogs from './OperationLogs';
 
-  const App = () => {
+const APP_KEY = 'app'
 
+  const App = () => {
     // rootReducerにて各Reducerはオブジェクト形式で保管されている
     // stateはそれぞれの状態を一元管理しているので、初期値に注意
-    const initialState = {
+    const appState = localStorage.getItem(APP_KEY)
+    const initialState = appState ? JSON.parse(appState) : {
       events: [],
       operationLogs: []
     };
     
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+      localStorage.setItem(APP_KEY, JSON.stringify(state))
+    },[state]);
+
 
     return (
     <AppContext.Provider value={{state, dispatch}}>
